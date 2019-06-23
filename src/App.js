91717c85d2b593f99f441678dbe13a66e38033d2
Lawson Zeugin <lawson@inif.io>
2019-06-23 16:44:32 +0300

    simple landing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Seperator"/>
+      <div className="NavBar">
+        <h1 className="PageTitle" >Inif</h1>
+        <div className="NavBarLinks">
+          {/* <a className="NavBarLink">Work</a>
+          <a className="NavBarLink">About</a>
+          <a className="NavBarLink">Contact</a> */}
+        </div>
+      </div>
+      <div className="Landing">
+        <div>
+          <h2 className="LandingBody">Hey there, 👋 my name's Lawson and I'm a software developer!</h2>
+          <h3>I'm currently building this site in my freetime, more to come! Please feel free to contact me at <a href="mailto:lawson@inif.io">lawson@inif.io</a></h3>
+        </div>
+      </div>
     </div>
   );
 }
